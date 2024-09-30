d8329693f0ff42917e2955e612066d086a677163
Konoto269 <Konoto269@gmail.com>
2024-09-30 14:22:11 -0700

    Update statEdit.js

--- commands/statEdit.js
+++ commands/statEdit.js
@@ -3,16 +3,24 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('Stat Edit')
-        .setDescription('Increase or decrease a stat value')
-        .addStringOption(option => 
-            option.setName('Stat')
-            .setDescription('The Stat you want to change')
-            .setRequired(true)
-        .addStringOption(option => 
-            option.setName('Value')
-            .setDescription('The amount to change by')
-            .setRequired(true),
-    async execute(interaction, client) {
-        interaction.reply('Pong!');
+        .setDescription('Increase or decrease a stat value'),
+    async execute(interaction) {
+        const characterName = interaction.options.getString('name');
+        const userId = interaction.user.id;
+
+        // checkfor an existing character
+        const existingCharacter = await Character.findOne({ userId});
+
+        if (!existingCharacter) {
+            return interaction.reply({
+                content: 'No Character.',
+                ephemeral: true
+            });
+        else {
+            return interaction.reply({
+                content: 'Incomplete.',
+                ephemeral: true
+            });
+        }
     }
-}
+} 
