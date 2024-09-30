9b16cf6fd0d436272086836bd829d20f7541ca76
Konoto269 <Konoto269@gmail.com>
2024-09-30 15:21:57 -0700

    Update statEdit.js

--- commands/statEdit.js
+++ commands/statEdit.js
@@ -15,10 +15,12 @@ module.exports = {
             .setRequired(true)
         ),
     async execute(interaction) {
+        const statName = interaction.options.getString('stat');
+        const statChange = interaction.options.getInteger('value');
         const userId = interaction.user.id;
 
         // checkfor an existing character
-        const existingCharacter = await Character.findOne({ userId});
+        const character = await Character.findOne({ userId});
 
         if (!existingCharacter) {
             return interaction.reply({
@@ -26,7 +28,23 @@ module.exports = {
                 ephemeral: true
             });
         }
-        
+        // Edit stats
+        if (statName == 'Str', 'str', 'Strength', 'strength') {
+            statName = 'strength';
+        } else if (statName == 'Dex', 'dex', 'Dexterity', 'dexterity') {
+            statName = 'dexterity';
+        } else if (statName == 'Con', 'con', 'Constitution', 'constitution') {
+            statName = 'constitution';
+        } else if (statName == 'Int', 'int', 'Intelligence', 'intelligence') {
+            statName = 'intelligence';
+        } else if (statName == 'Gold', 'gold') {
+            statName = 'gold';
+        } else {
+            return interaction.reply({
+                content: 'No Character.',
+                ephemeral: true
+            });
         }
+        
     }
 } 
