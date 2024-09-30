3b3e3e3fd576d75b7abdeb53d95e5f1618e62458
Konoto269 <Konoto269@gmail.com>
2024-09-30 13:58:15 -0700

    Update statEdit.js

--- commands/statEdit.js
+++ commands/statEdit.js
@@ -3,7 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('Stat Edit')
-        .setDescription('Pong!'),
+        .setDescription('Increase or decrease a stat value')
+        .addStringOption(option => 
+            option.setName('Stat')
+            .setDescription('The Stat you want to change')
+            .setRequired(true)
+        .addStringOption(option => 
+            option.setName('Value')
+            .setDescription('The amount to change by')
+            .setRequired(true),
     async execute(interaction, client) {
         interaction.reply('Pong!');
     }
