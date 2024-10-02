13a592eeedfd54a91b600f7ae57c733a7e4f56c1
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-02 15:51:39 -0700

    Update leaveButton.js

--- buttons/leaveButton.js
+++ buttons/leaveButton.js
@@ -1,6 +1,22 @@
+const Character = require('../schemas/char'); // Import the character model
+const Tower = require('../schemas/tower'); //Import the Tower Entry model
+
 module.exports = {
 	customID: 'leaveBtn',
 	async execute(interaction, client) {
-		
+		const userId = interaction.user.id;
+        
+        // Find the user's character and entry
+		const character = await Character.findOne({ userId});
+        const entry = await Tower.findOne({ userId });
+
+		entry.currentFloor = 0;
+		entry.encounter = 0;
+		await entry.save();
+
+		await interaction.update({
+			content: `${character.characterName} has left the Tower.`,
+			components: [],
+		});
 	}
 }
