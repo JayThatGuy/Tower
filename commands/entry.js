f9804815e52c56cdcd91b1b8ab3f5c801d1388a4
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-02 15:54:04 -0700

    Update entry.js

--- commands/entry.js
+++ commands/entry.js
@@ -45,42 +45,61 @@ module.exports = {
             await entry.save();
         }
 
-	const rightButton = new ButtonBuilder()
-	.setCustomId('rightBtn')
-	.setStyle(ButtonStyle.Primary)
-	.setLabel('Right');
-	const middleButton = new ButtonBuilder()
-	.setCustomId('middleBtn')
-	.setStyle(ButtonStyle.Primary)
-	.setLabel('Middle');
-	const leftButton = new ButtonBuilder()
-	.setCustomId('leftBtn')
-	.setStyle(ButtonStyle.Primary)
-	.setLabel('Left');
+	    const rightButton = new ButtonBuilder()
+	    .setCustomId('rightBtn')
+	    .setStyle(ButtonStyle.Primary)
+	    .setLabel('Right');
+	    const middleButton = new ButtonBuilder()
+	    .setCustomId('middleBtn')
+	    .setStyle(ButtonStyle.Primary)
+	    .setLabel('Middle');
+        const leftButton = new ButtonBuilder()
+        .setCustomId('leftBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Left');
+        const retreatButton = new ButtonBuilder()
+        .setCustomId('retreatBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Retreat');
+        
         const leaveButton = new ButtonBuilder()
-	.setCustomId('leaveBtn')
-	.setStyle(ButtonStyle.Primary)
-	.setLabel('Leave');
+        .setCustomId('leaveBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Leave');
+        const forwardButton = new ButtonBuilder()
+        .setCustomId('forwardBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Press Forward');
 
-	const row1 = new ActionRowBuilder()
-	.addComponents(rightButton, middleButton, leftButton);
+        const row1 = new ActionRowBuilder()
+        .addComponents(leftButton, middleButton, rightButton, retreatButton);
+        const row2 = new ActionRowBuilder()
+        .addComponents(forwardButton, leaveButton);
 
-	const leftDoor = doorColor(getRandomInt(3));
-	const rightDoor = doorColor(getRandomInt(3));
-	const middleDoor = doorColor(getRandomInt(3));
+        //saves the door options
+        entry.leftDoor = getRandomInt(3);
+        entry.middleDoor = getRandomInt(3);
+        entry.rightDoor = getRandomInt(3);
+        await entry.save();
+        
+        //generates the door type
+        const leftDoor = doorColor(entry.leftDoor);
+        const rightDoor = doorColor(entry.rightDoor);
+        const middleDoor = doorColor(entry.middleDoor);
 		
-	if (entry.currentFloor == 0){
-        	var row2 = new ActionRowBuilder()
-		.addComponents(leaveButton);
-            	await interaction.reply({
-                	content: '`The left door is **${leftDoor}**. The middle door is ${middleDoor}. The right door is ${rightDoor}.`',
-                	components: [row1, row2],
-            	});
-        }else{
-        	await interaction.reply({
-			content: '`The left door is **${leftDoor}**. The middle door is ${middleDoor}. The right door is ${rightDoor}.`',
-			components: [row1],
-		});
+	    if (entry.currentFloor == 0){
+            await interaction.reply({
+                content: `Floor ${entry.currentFloor}\nThe doors of the tower are open to explore.`,
+                components: [row2],
+            });
         }
+        if (entry.currentFloor > 0 && entry.encounter < 3){
+        	await interaction.reply({
+			content: `Floor ${entry.currentFloor}\nThe left door is **${leftDoor}**. The middle door is **${middleDoor}**. The right door is **${rightDoor}**.`,
+			components: [row1],
+		    });
+        }
+        
     }
+    
 }
