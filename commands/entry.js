f97399205e5700f3db3d604bc3246c8efbea4b0e
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-01 18:08:01 -0700

    Update entry.js

--- commands/entry.js
+++ commands/entry.js
@@ -3,12 +3,10 @@ const { EmbedBuilder } = require('discord.js');
 const Character = require('../schemas/char'); // Import the character model
 const Tower = require('../schemas/tower'); // Import the tower model
 
-// rng
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
-// determine door type
 function doorColor(roll) {
 	if (roll == 0) {
 		return ('Red');
@@ -25,39 +23,64 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('enter')
 		.setDescription('Enter the Gungeon!'),
-	// create buttons
 	async execute(interaction, client) {
-		const leftButton = new ButtonBuilder()
-		.setCustomId('leftBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Left');
-		const middleButton = new ButtonBuilder()
-		.setCustomId('middleBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Middle');
-		const rightButton = new ButtonBuilder()
-		.setCustomId('rightBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Right');
-        	const leaveButton = new ButtonBuilder()
-		.setCustomId('leaveBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Leave');
+        const userId = interaction.user.id;
 
-		const row1 = new ActionRowBuilder()
-		.addComponents(rightButton, middleButton, leftButton);
-        	const row2 = new ActionRowBuilder()
+        // Find the user's character
+        const character = await Character.findOne({ userId});
+
+        if (!character) {
+            return interaction.reply({
+                content: 'You do not have a character created. Use `/character` to create one.',
+                ephemeral: true
+            });
+        }
+
+        // Retrieve or create the user's profile with gold
+        let entry = await Tower.findOne({ userId });
+        if (!entry) {
+            entry = new Tower({ 
+                userId: userId,
+            });
+            await entry.save();
+        }
+
+	const rightButton = new ButtonBuilder()
+	.setCustomId('rightBtn')
+	.setStyle(ButtonStyle.Primary)
+	.setLabel('Right');
+	const middleButton = new ButtonBuilder()
+	.setCustomId('middleBtn')
+	.setStyle(ButtonStyle.Primary)
+	.setLabel('Middle');
+	const leftButton = new ButtonBuilder()
+	.setCustomId('leftBtn')
+	.setStyle(ButtonStyle.Primary)
+	.setLabel('Left');
+        const leaveButton = new ButtonBuilder()
+	.setCustomId('leaveBtn')
+	.setStyle(ButtonStyle.Primary)
+	.setLabel('Leave');
+
+	const row1 = new ActionRowBuilder()
+	.addComponents(rightButton, middleButton, leftButton);
+
+	const leftDoor = doorColor(getRandomInt(3));
+	const rightDoor = doorColor(getRandomInt(3));
+	const middleDoor = doorColor(getRandomInt(3));
+		
+	if (entry.currentFloor == 0){
+        	var row2 = new ActionRowBuilder()
 		.addComponents(leaveButton);
-
-		//generate doors
-		const leftDoor = doorColor(getRandomInt(3));
-		const rightDoor = doorColor(getRandomInt(3));
-		const middleDoor = doorColor(getRandomInt(3));
-
-		await interaction.reply({
-			content: `The left door is **${leftDoor}**. The middle door is ${middleDoor}. The right door is ${rightDoor}.`,
-			components: [row1, row2],
+            	await interaction.reply({
+                	content: '`The left door is **${leftDoor}**. The middle door is ${middleDoor}. The right door is ${rightDoor}.`',
+                	components: [row1, row2],
+            	});
+        }else{
+        	await interaction.reply({
+			content: '`The left door is **${leftDoor}**. The middle door is ${middleDoor}. The right door is ${rightDoor}.`',
+			components: [row1],
 		});
-
-	}
+        }
+    }
 }
