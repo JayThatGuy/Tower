d0ba2cf363a99a23ed4822eb2c41460f8341e18e
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-02 15:53:17 -0700

    Add files via upload

---
+++ buttons/retreatButton.js
@@ -0,0 +1,9 @@
+const Character = require('../schemas/char'); // Import the character model
+const Tower = require('../schemas/tower'); //Import the Tower Entry model
+
+module.exports = {
+    customID: 'retreatBtn',
+    async execute(interaction, client) {
+		
+	}
+}
