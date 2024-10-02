3da7f90e64f9983fbbecc3c8b037f55d8dc61930
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-01 23:11:56 -0700

    Update forwardButton.js

--- buttons/forwardButton.js
+++ buttons/forwardButton.js
@@ -1,6 +1,46 @@
+const Tower = require('../schemas/tower');
+
 module.exports = {
 	customID: 'forwardBtn',
 	async execute(interaction, client) {
-		
+		const userId = interaction.user.id;
+        
+        // Find the user's character
+        const entry = await Tower.findOne({ userId });
+
+        const rightButton = new ButtonBuilder()
+	    .setCustomId('rightBtn')
+	    .setStyle(ButtonStyle.Primary)
+	    .setLabel('Right');
+	    const middleButton = new ButtonBuilder()
+	    .setCustomId('middleBtn')
+	    .setStyle(ButtonStyle.Primary)
+	    .setLabel('Middle');
+        const leftButton = new ButtonBuilder()
+        .setCustomId('leftBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Left');
+        
+        const leaveButton = new ButtonBuilder()
+        .setCustomId('leaveBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Leave');
+        const forwardButton = new ButtonBuilder()
+        .setCustomId('forwardBtn')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel('Press Forward');
+
+        
+        
+		entry.currentFloor += 1;
+        if(entry.currentFloor > entry.highestFloor){
+            entry.highestFloor = entry.currentFloor;
+        }
+		await entry.save();
+
+        await interaction.update({
+            content: `Going up.`,
+            components: [],
+        })
 	}
 }
