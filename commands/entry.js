00362b271cd8c4f0b4e36104d012a1263dbe0867
Konoto269 <Konoto269@gmail.com>
2024-10-01 17:29:10 -0700

    Update entry.js

--- commands/entry.js
+++ commands/entry.js
@@ -8,26 +8,26 @@ module.exports = {
 		.setName('enter')
 		.setDescription('Enter the Gungeon!'),
 	async execute(interaction, client) {
-		const rightButton = new ButtonBuilder()
-		.setCustomId('rightBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Right');
-		const middleButton = new ButtonBuilder()
-		.setCustomId('middleBtn')
-		.setStyle(ButtonStyle.Primary)
-		.setLabel('Middle');
 		const leftButton = new ButtonBuilder()
 		.setCustomId('leftBtn')
 		.setStyle(ButtonStyle.Primary)
 		.setLabel('Left');
-        const leaveButton = new ButtonBuilder()
+		const middleButton = new ButtonBuilder()
+		.setCustomId('middleBtn')
+		.setStyle(ButtonStyle.Primary)
+		.setLabel('Middle');
+		const rightButton = new ButtonBuilder()
+		.setCustomId('rightBtn')
+		.setStyle(ButtonStyle.Primary)
+		.setLabel('Right');
+        	const leaveButton = new ButtonBuilder()
 		.setCustomId('leaveBtn')
 		.setStyle(ButtonStyle.Primary)
 		.setLabel('Leave');
 
 		const row1 = new ActionRowBuilder()
 		.addComponents(rightButton, middleButton, leftButton);
-        const row2 = new ActionRowBuilder()
+        	const row2 = new ActionRowBuilder()
 		.addComponents(leaveButton);
 
 		await interaction.reply({
