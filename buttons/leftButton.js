6693c0565732825f7522a61559de19e2a17f6a1a
JayThatGuy <103267137+JayThatGuy@users.noreply.github.com>
2024-10-01 20:43:55 -0700

    Update leftButton.js

--- buttons/leftButton.js
+++ buttons/leftButton.js
@@ -1,10 +1,18 @@
+const Tower = require('../schemas/tower'); // Import the tower model
+
+
 module.exports = {
 	customID: 'leftBtn',
 	async execute(interaction, client) {
-		tower.encounter +=;
-		await tower.save();
+        const userId = interaction.user.id;
+        
+        // Find the user's character
+        const entry = await Tower.findOne({ userId });
+
+		entry.encounter += 1;
+		await entry.save();
 		await interaction.reply({
-			content: `Welcome to encounter ${tower.encounter}`,
+			content: `Welcome to encounter ${entry.encounter}`,
 			ephemeral: true
 		});
 		
