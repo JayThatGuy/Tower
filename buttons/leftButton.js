8538f404d973001f93a5776450e700a721584470
Konoto269 <Konoto269@gmail.com>
2024-10-01 20:32:53 -0700

    Update leftButton.js

--- buttons/leftButton.js
+++ buttons/leftButton.js
@@ -2,7 +2,7 @@ module.exports = {
 	customID: 'leftBtn',
 	async execute(interaction, client) {
 		tower.encounter +=;
-		await tower.save;
+		await tower.save();
 		await interaction.reply({
 			content: `Welcome to encounter ${tower.encounter}`,
 			ephemeral: true
